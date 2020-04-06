9e21ff632dd97b26c305078ae485e3c4584f7582
unknown <binhu10@gmail.com>
2020-04-05 23:58:33 -0400

    testing writing and delete

--- server.js
+++ server.js
@@ -31,14 +31,14 @@ app.get("/api/notes", function(req, res) {
         console.log(err);
     }
 
-    res.JSON(notesData);
+    res.json(notesData);
 });
 
 // write new notes to JSON file
 
 app.post("/api/notes", function(req, res) {
     try {
-        notesData = fs.readFileSync("./Develop/db/db/json", "utf8");
+        notesData = fs.readFileSync("./Develop/db/db.json", "utf8");
         console.log(notesData);
 
         notesData = JSON.parse(notesData);
@@ -47,7 +47,7 @@ app.post("/api/notes", function(req, res) {
 
         notesData = JSON.stringify(notesData);
 
-        fs.writeFile("./Develop/db/db.json", "utf8", function(err) {
+        fs.writeFile("./Develop/db/db.json", notesData, "utf8", function(err) {
             if (err) throw err;
         });
         res.json(JSON.parse(notesData));
@@ -57,6 +57,36 @@ app.post("/api/notes", function(req, res) {
     } 
 })
 
+// delete Note
+
+app.delete("/api/notes/:id", function(req, res) {
+    try {
+      //  reads the json file
+      notesData = fs.readFileSync("./Develop/db/db.json", "utf8");
+      
+      notesData = JSON.parse(notesData);        
+      // delete the old note from the array on note objects
+      notesData = notesData.filter(function(note) {
+        return note.id != req.params.id;
+        
+      });
+      // make it string(stringify)so you can write it to the file
+      notesData = JSON.stringify(notesData);
+      // write the new notes to the file
+      fs.writeFile("./Develop/db/db.json", notesData, "utf8", function(err) {
+        // error handling
+        if (err) throw err;
+      });
+  
+      // change it back to an array of objects & send it back to the browser (client)
+      res.send(JSON.parse(notesData));
+  
+      // error handling
+    } catch (err) {
+      throw err;
+      console.log(err);
+    }
+  });
 
 
 //Web page HTML

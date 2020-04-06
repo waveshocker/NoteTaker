9c20406034d1e9757b333994750d5ebca4e723a3
unknown <binhu10@gmail.com>
2020-04-06 00:34:41 -0400

    changes to how notes is take and created class to streamline process

---
+++ Develop/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const htmlRoutes = require("./routes/htmlRoute");
+const apiRoutes = require("./routes/apiRoute");
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
+
+app.listen(PORT,()=> console.log(`Listening on PORT : ${PORT}`));
